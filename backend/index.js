83f378714cfa1c408e4fd1dd4e9b88656d3380d0
Sanjay Raj <sanjayraj23560@gmail.com>
2026-05-10 23:05:27 +0530

    fix: mongodb connection startup issue for render

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,9 @@ const Email = mongoose.model("mngEmails", EmailSchema);
 ========================= */
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
